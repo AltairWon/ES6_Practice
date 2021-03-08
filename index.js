03a7b10b0b04c7b6857d725f2674502ca3c50df0
repl.it user <replituser@example.com>
2021-03-08 08:04:51 +0000

    #1.7 let, const, var

---
+++ index.js
@@ -0,0 +1,7 @@
+//console.log('Im Working. Im JS. Im Beautiful. Im worth it');
+//alert: 경고 메시지 Hello world라고 불린다.
+const a = 221; //const 변수가 바꿀 수 없게 하려는 목적이 있다.
+let b = a-5; //let 선언 변수의 종류가 두가지라 이를 구분하기 위해서 사용
+a = 4;
+var c // var = variable 이상한 규칙이 있는데 상관은 없다
+console.log(b, a);
