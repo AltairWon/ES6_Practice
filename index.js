bfff6c2220bfcca6bb7017c8d91b68da236fb7b0
repl.it user <replituser@example.com>
2021-03-09 02:02:40 +0000

    #2.2 JS DOM Functions

--- index.js
+++ index.js
@@ -27,24 +27,45 @@ const wat = 55.1;
 //Object를 선언하기 위해서는 { 컬리 브라켓 }을 사용한다
 //실제 객체를 만든다
 //Array를 Object안에 넣어도 상관 없다.
+/*
 const nicoInfo = {
   name: "Nico",
   age: 33,
   gender: "Male",
   isHandsome: true,
-  favMovies: ["Along the gods", "LOTR", "Oldboy"],
-  favFood: [
-    {
-      name: "Kimchi", 
-      fatty:false
-    }, 
-    {
-      name: "Cheese Burger",
-      fatty: "true"
-    }
-  ]
 }
 
 //Object를 보기 위해서는 이름 옆에 . 을 붙인다.
-console.log(nicoInfo.favFood[0].fatty);
+//console은 Object이다 더 큰 Object를 뜻한다.
+//console, alert은 내장함수라고 불린다.
+//console.log(nicoInfo, console);
 
+//함수를 작성해서 사용하기
+//함수는 인자 argument를 받는다
+function sayHello(name, age) {
+  //console.log("Hello!", name, "you have", age, "years of age.");
+  return `Hello ${name} you are ${age} years old`;
+}
+
+//greetAltair는 sayHello의 실행된 결과 값이다.
+const greetAltair = sayHello("Altair", 15);
+
+//이렇게 하면 undefined로 나오는데 return 값이 없기 때문이다.
+//return을 사용하면 괜춘하다
+console.log(greetAltair)
+
+const calculator = {
+  plus: function(a, b) {
+    return a + b;
+  }
+}
+
+const plus = calculator.plus(5, 5)
+console.log(plus)
+*/
+
+//getElementById는 html안에 있는 title이라는 id를 찾도록 하는 함수 
+const title = document.getElementById("title");
+title.innerHTML = "Hi! from JS";
+console.log(title);
+//DOM = Document Object Module
