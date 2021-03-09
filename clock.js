a677dbf1e14af34fddfb5a18b1333fb38f4e8700
whj121 <whj121@estorm.co.kr>
2021-03-09 15:20:42 +0900

    #3.1 Making a JS Clock part Two

--- clock.js
+++ clock.js
@@ -6,11 +6,17 @@ function getTime(){
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+    //ternary operator(삼항연산자) = 작은 if문 -> ? : 기억하자
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours
+    }:${minutes < 10 ? `0${minutes}`: minutes
+    }:${seconds < 10 ? `0${seconds}` : seconds
+    }`;
 }
 
 function init() {
     getTime();
+    //setInterval = 시간을 정해서 계속해서 보여주게 만드는 함수
+    setInterval(getTime, 1000);
 }
 
 init();
